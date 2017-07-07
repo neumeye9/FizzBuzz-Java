a2a58b1224c2902871c205de7db22bddb0348b71
neumeye9 <krista.neumeyer@gmail.com>
2017-07-07 15:47:27 -0400

    initial commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz fB = new FizzBuzz();
+        String answer = fB.fizzBuzz(10);
+        System.out.println(answer);
+    }
+}
